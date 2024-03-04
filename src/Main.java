88b0f2dea0c54698f546e590513ef02ee35fb7da
j-stapera <haltheretic@gmail.com>
2024-03-04 06:54:24 -0600

    Used code from BroCode Video (see Main for link)

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+
+    public static void main(String[] args) {
+        //code from https://www.youtube.com/watch?v=tHNWIWxRDDA
+        //testing to see if I can even get things to work
+        new MyFrame();
+    }
+
+}
